ff140cbb97ee287a0901067073931e8a3d1e49da
Omersmsk1 <omersmsk34@gmail.com>
2026-06-01 19:29:01 +0300

    Dashboard sayfasi eklendi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+
 import Giris from "./pages/Giris";
 import KayitOl from "./pages/KayitOl";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Giris />} />
       <Route path="/kayit" element={<KayitOl />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,31 @@
+import "./Dashboard.css";
+
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="dashboard-container">
+      <h1>Rubis Kütüphane Sistemi</h1>
+
+      <div className="dashboard-grid">
+
+        <div className="card">
+          <h2>📚 Kitaplar</h2>
+        </div>
+
+        <div className="card">
+          <h2>📖 Ödünç Al</h2>
+        </div>
+
+        <div className="card">
+          <h2>📥 Kitap Teslim</h2>
+        </div>
+
+        <div className="card">
+          <h2>🚪 Çıkış</h2>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
